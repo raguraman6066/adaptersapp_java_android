d036ce3e4420a34a9eb611e758e9a13efd75544c
ragu raman <raguraman.mps6066@gmail.com>
2024-02-22 16:39:23 +0530

    notes commit

--- app/src/main/java/com/example/adaptersapp/MainActivity.java
+++ app/src/main/java/com/example/adaptersapp/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+/*
+* adapter connects user interface and data sources
+* bridget between adapter view and data
+*1. need adapter view(like listview, grid view...), 2. data source  3.adapter
+* */
 public class MainActivity extends AppCompatActivity {
 ListView listView;
     @Override
@@ -13,14 +17,22 @@ ListView listView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //adapter - bridge bt ui and data source
+        //1.adapter view (listview here) - used to show list of items ex-listview, gridview..
         listView=findViewById(R.id.listView);
-        //data source
-        String[] countries={"USA","Germany","France","Saudi Arabia"};
+        //2.data source - collection of strings
+        String[] countries=new String[]{"USA","Germany","France","Saudi Arabia"};
 
-        //adapter - data to ui elements
+        //3.adapter - binding data to views within adapter view (list view)
+        //common adapters- array adapter- bind list of data to view-simple string
+        //pre-defined layout for one single item - simple_list_item_1
+        //using adapter we convert list into individual ui elements and then put to listview
+//''''''''''''''''''''''''''
        // ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,countries);
+       // listView.setAdapter(adapter);
+                  //or
+//another type of adapter ..base adapter -more customizable-use subclass
        MyCustomAdapter adapter=new MyCustomAdapter(this,countries);
-
-        listView.setAdapter(adapter);
+       listView.setAdapter(adapter);
     }
 }
+
--- app/src/main/java/com/example/adaptersapp/MyCustomAdapter.java
+++ app/src/main/java/com/example/adaptersapp/MyCustomAdapter.java
@@ -6,48 +6,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
+// 4 methods need to implement
 public class MyCustomAdapter extends BaseAdapter {
+    //context-bridget between application code and os , allow u to interact with system,access resources
     private Context context;
+    //data source
     private String[] items;
-
+//constructor
     public MyCustomAdapter(Context context, String[] items) {
         this.context = context;
         this.items = items;
     }
 
+
+    //1.return number of items in data source, tell adapter to create how many items it should create views
     @Override
     public int getCount() {
         return items.length;
     }
-
+    //2.get data item at specific position within our data source
     @Override
     public Object getItem(int position) {
         return items[position];
     }
-
+    //3. returns a unique id for the item at the given position
     @Override
     public long getItemId(int position) {
         return position;
     }
-
+    //4. displays the data at a position in the data set - creating and returning the view
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if(convertView==null){
+        //convertview-is a recycled view that you can reuse to improve the performance of your list.
+        ViewHolder holder;//static view holder class
+        if(convertView==null){//create new view
            convertView= LayoutInflater.from(context).inflate(R.layout.my_list_item,parent,false);
            holder=new ViewHolder();
            holder.textView=convertView.findViewById(R.id.tv);
            convertView.setTag(holder);
         }else{
             //reuse the view
-            holder=(ViewHolder) convertView.getTag();
+            holder=(ViewHolder) convertView.getTag();//gettag-get previous viewholder objects
         }
         holder.textView.setText(items[position]);
         return convertView;
     }
 
-    static  class ViewHolder{
+    static class ViewHolder{
         TextView textView;
     }
 }
